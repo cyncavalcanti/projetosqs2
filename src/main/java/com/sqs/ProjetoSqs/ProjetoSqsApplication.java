ac7420c6ded481b09c00ce92c434924d5667a526
Cynthia Cavalcanti <cynthia.cavalcanti123@gmail.com>
2022-12-02 19:22:37 -0300

    Start projeto

---
+++ src/main/java/com/sqs/ProjetoSqs/ProjetoSqsApplication.java
@@ -0,0 +1,13 @@
+package com.sqs.ProjetoSqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqs/ProjetoSqs/ProjetoSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqs.ProjetoSqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
